c937baf7b2898e6d61fbe9990f39a08f5239e638
reiji <reiji@192.168.2.102>
2024-07-10 16:09:59 +0900

    Test_07_10_home

---
+++ demo/src/demo/Test_07_10_job.java
@@ -0,0 +1,25 @@
+package demo;
+
+import umejava_05_01.Interface;
+
+public class Test_07_10_job 
+{
+	public static void main( String[] args )
+	{
+		Calc10 calc = new Calc10( "Poly-morphism" );
+			calc.process( () -> {
+				System.out.println( "関心事を記述" );
+				
+				Interface save = Calc10.create();
+					save.execute();
+			});
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
