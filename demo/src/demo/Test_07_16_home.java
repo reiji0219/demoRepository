c796c04ddf6efcd22e6c026c65544e21c0d9c4cc
reiji <reiji@192.168.2.102>
2024-07-16 08:32:22 +0900

    Test_07_16_home

---
+++ demo/src/demo/Test_07_16_home.java
@@ -0,0 +1,25 @@
+package demo;
+
+import umejava_05_01.Interface;
+
+public class Test_07_16_home 
+{
+	public static void main( String[] args )
+	{
+		Calc16 calc = new Calc16( "Poly-morphism" );
+			calc.process( () -> {
+				System.out.println( "関心事を記述" );
+				
+				Interface save = Calc16.create();
+					save.execute();
+			});
+			
+		
+		
+		
+		
+		
+		
+	}
+	
+}
