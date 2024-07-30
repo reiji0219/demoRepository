8444a7fc7b82845b73435e995e518d9dc924d630
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-07-31 08:22:43 +0900

    Comparator -> Lambdaで記述

--- demo/src/demo/Item31.java
+++ demo/src/demo/Item31.java
@@ -1,17 +1,16 @@
 package demo;
 
+import java.util.Iterator;
+import java.util.Collections;
+import java.util.Comparator; 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 import umejava_05_01.Interface;
 
 public class Item31 implements Interface
-{
-	@Override
-	public void execute() {
-		indi( "Lambda" );
-		
-	}
-	
+{	
 	private String id;
 	private String name;
 	private double price;
@@ -70,5 +69,70 @@ public class Item31 implements Interface
 	public static void indi( String s0 ) {
 		System.out.println( s0 );
 	}
-	
+	@Override
+	public void execute() {
+		indi( "Lambda" );
+		
+		ArrayList<String> animals = new ArrayList<String>( Arrays.asList( "Dog" ));
+		
+		ArrayList<String> animalList = new ArrayList<String>();
+			animalList.add( "Cat" );
+			animalList.add( "Lion" );
+			animalList.add( "Penguin" );
+			animalList.add( "Gorrila" );
+			
+			animals.addAll( animalList );
+
+		String getAnimal = animals.get( 4 );
+			indi( getAnimal );
+			
+			animals.set( 4, "Fox" );
+			
+			animals.remove( 0 );
+			animals.add( 0, "Doragon" );
+/*			//normal			
+		Comparator<String> compa = new Comparator<String>() {
+			@Override
+			public int compare( String o1, String o2 ) {
+				return( o2.compareTo( o1 ));
+			}
+		};
+			// 匿名クラスを使用
+		Comparator<String> compa = new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return( o2.compareTo( o1 ));
+			}
+		};
+*/
+			// Lambda式で記述
+		Comparator<String> compa = (  o1,  o2 ) -> {
+			return( o2.compareTo( o1 ));
+			};
+		
+			
+		Collections.sort( animals, compa );	
+			
+		Iterator<String> itera = animals.iterator();
+		while( itera.hasNext()) {
+			String hoge = itera.next();
+				indi( hoge );
+		}
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
